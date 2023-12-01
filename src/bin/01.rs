338da5614e985d2e186b6874398879836169a706
Dylan Starink <dylan@starink.com>
2023-11-30 21:55:34 -0800

    Cargo fmt

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,52 +5,78 @@ use itertools::Itertools;
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.trim().lines().map(|line| {
-        let chars = line.chars().into_iter();
-        
-        let first = line.chars().into_iter().find(|c| c.is_digit(10))
-            .expect("Numeric Char always exist");
+    Some(
+        input
+            .trim()
+            .lines()
+            .map(|line| {
+                let first = line
+                    .chars()
+                    .find(|c| c.is_ascii_digit())
+                    .expect("Numeric Char always exist");
 
-        let last = line.chars().into_iter().rev().find(|c| c.is_digit(10))
-            .expect("Numeric Char always exist");
+                let last = line
+                    .chars()
+                    .rev()
+                    .find(|c| c.is_ascii_digit())
+                    .expect("Numeric Char always exist");
 
-        let full_num = String::from_iter(vec![first, last].iter());
+                let full_num = String::from_iter(vec![first, last].iter());
 
-        let parsed_num: u32 = full_num.parse().expect("Number always valid");
+                let parsed_num: u32 = full_num.parse().expect("Number always valid");
 
-        parsed_num
-        
-    }).sum())
+                parsed_num
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let valid_words = HashMap::from([("one", 1), ("two", 2), ("three", 3), 
-    ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]);
+    let valid_words = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
 
-    Some(input.trim().lines().map(|line| {
-        // Find all digits and possible words and find the first occurance of one
-        let numbers = valid_words.iter()
-            .flat_map(|(word, value)| line.match_indices(word).map(|mtch| (mtch.0, *value)))
-            .chain(
-                line.chars()
-                    .into_iter()
-                    .enumerate()
-                    .filter_map(|(idx, c)| {
-                        if c.is_digit(10) {
+    Some(
+        input
+            .trim()
+            .lines()
+            .map(|line| {
+                // Find all digits and possible words and find the first occurance of one
+                let numbers = valid_words
+                    .iter()
+                    .flat_map(|(word, value)| line.match_indices(word).map(|mtch| (mtch.0, *value)))
+                    .chain(line.chars().enumerate().filter_map(|(idx, c)| {
+                        if c.is_ascii_digit() {
                             Some((idx, c.to_digit(10).unwrap()))
                         } else {
                             None
                         }
-                    })
-              );
+                    }));
 
-        
-        let first = numbers.clone().min_by_key(|i| i.0).expect("At least one match").1;
+                let first = numbers
+                    .clone()
+                    .min_by_key(|i| i.0)
+                    .expect("At least one match")
+                    .1;
 
-        let last = numbers.clone().max_by_key(|i| i.0).expect("At least one match").1;
+                let last = numbers
+                    .clone()
+                    .max_by_key(|i| i.0)
+                    .expect("At least one match")
+                    .1;
 
-        first * 10 + last
-    }).sum())
+                first * 10 + last
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
