5947b541cb9f0f1fde6242695de7dc1f23f7b5a4
Dylan Starink <dylan@starink.com>
2023-11-30 21:53:18 -0800

    Day 1 Complete!

---
+++ src/bin/01.rs
@@ -0,0 +1,71 @@
+use std::{collections::HashMap, iter};
+
+use itertools::Itertools;
+
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(input.trim().lines().map(|line| {
+        let chars = line.chars().into_iter();
+        
+        let first = line.chars().into_iter().find(|c| c.is_digit(10))
+            .expect("Numeric Char always exist");
+
+        let last = line.chars().into_iter().rev().find(|c| c.is_digit(10))
+            .expect("Numeric Char always exist");
+
+        let full_num = String::from_iter(vec![first, last].iter());
+
+        let parsed_num: u32 = full_num.parse().expect("Number always valid");
+
+        parsed_num
+        
+    }).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let valid_words = HashMap::from([("one", 1), ("two", 2), ("three", 3), 
+    ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]);
+
+    Some(input.trim().lines().map(|line| {
+        // Find all digits and possible words and find the first occurance of one
+        let numbers = valid_words.iter()
+            .flat_map(|(word, value)| line.match_indices(word).map(|mtch| (mtch.0, *value)))
+            .chain(
+                line.chars()
+                    .into_iter()
+                    .enumerate()
+                    .filter_map(|(idx, c)| {
+                        if c.is_digit(10) {
+                            Some((idx, c.to_digit(10).unwrap()))
+                        } else {
+                            None
+                        }
+                    })
+              );
+
+        
+        let first = numbers.clone().min_by_key(|i| i.0).expect("At least one match").1;
+
+        let last = numbers.clone().max_by_key(|i| i.0).expect("At least one match").1;
+
+        first * 10 + last
+    }).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples/part1", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples/part2", DAY));
+        assert_eq!(result, Some(281));
+    }
+}
