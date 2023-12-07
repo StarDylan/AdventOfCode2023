2e615e05aa12c396217702467739874900437ac1
Dylan Starink <dylan@starink.com>
2023-12-06 22:27:45 -0800

    Clippy fmt lints

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -5,7 +5,7 @@ advent_of_code::solution!(5);
 struct Mapping {
     dest_start: u64,
     source_start: u64,
-    range_len: u64
+    range_len: u64,
 }
 
 impl Mapping {
@@ -25,52 +25,66 @@ struct SeedRange {
 
 impl SeedRange {
     pub fn is_in_range(&self, value: u64) -> bool {
-        self.start <= value && value < self.start + self.length 
+        self.start <= value && value < self.start + self.length
     }
 }
 
 #[derive(Debug, Clone)]
 struct MapFunction {
-    funcs: Vec<Mapping>
+    funcs: Vec<Mapping>,
 }
 
 impl Mapping {
     pub fn from_line(s: &str) -> Mapping {
         let mut parts = s.split_ascii_whitespace();
 
-        Mapping { 
-            dest_start: parts.next().expect("always exists").parse().expect("Always u32"), 
-            source_start: parts.next().expect("always exists").parse().expect("Always u32"), 
-            range_len: parts.next().expect("always exists").parse().expect("Always u32") 
+        Mapping {
+            dest_start: parts
+                .next()
+                .expect("always exists")
+                .parse()
+                .expect("Always u32"),
+            source_start: parts
+                .next()
+                .expect("always exists")
+                .parse()
+                .expect("Always u32"),
+            range_len: parts
+                .next()
+                .expect("always exists")
+                .parse()
+                .expect("Always u32"),
         }
     }
 }
 
 impl MapFunction {
     pub fn apply_function(&self, input: u64) -> u64 {
-        
-        let maybe_relevant_func = self.funcs.iter()
-            .find(|mapping| mapping.source_start <= input && input < mapping.source_start + mapping.range_len);
+        let maybe_relevant_func = self.funcs.iter().find(|mapping| {
+            mapping.source_start <= input && input < mapping.source_start + mapping.range_len
+        });
 
         match maybe_relevant_func {
             Some(relevant_func) => {
                 let offset = input - relevant_func.source_start;
                 relevant_func.dest_start + offset
-            },
+            }
             None => input,
         }
     }
 
-    
     pub fn reverse(self) -> Self {
         Self {
-            funcs: self.funcs.into_iter().map(|f| f.reverse()).collect_vec()
+            funcs: self.funcs.into_iter().map(|f| f.reverse()).collect_vec(),
         }
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let initial_seeds = input.lines().next().expect("always has a first line")
+    let initial_seeds = input
+        .lines()
+        .next()
+        .expect("always has a first line")
         .split(": ")
         .nth(1)
         .expect("Always a num list part")
@@ -78,62 +92,88 @@ pub fn part_one(input: &str) -> Option<u64> {
         .map(|s| s.parse::<u64>().expect("Every number is valid u32"))
         .collect_vec();
 
-    let maps = input.split("\n\n")
+    let maps = input
+        .split("\n\n")
         .skip(1) // Skip initial seed
         .map(|map_func| {
-            let funcs = map_func.lines().skip(1) // Skip title
+            let funcs = map_func
+                .lines()
+                .skip(1) // Skip title
                 .map(Mapping::from_line)
                 .collect_vec();
-            MapFunction {funcs}
-        }).collect_vec();
+            MapFunction { funcs }
+        })
+        .collect_vec();
 
     let final_ids = maps.iter().fold(initial_seeds, |ids, mapping_func| {
-        let ids = ids.iter().map(|id| mapping_func.apply_function(*id)).collect_vec();
+        let ids = ids
+            .iter()
+            .map(|id| mapping_func.apply_function(*id))
+            .collect_vec();
         ids
     });
 
-    let lowest_location = final_ids.iter()
-        .min().expect("Always at least one initial seed");
-
-    
+    let lowest_location = final_ids
+        .iter()
+        .min()
+        .expect("Always at least one initial seed");
 
     Some(*lowest_location)
-
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let initial_seeds = input.lines().next().expect("always has a first line")
+    let initial_seeds = input
+        .lines()
+        .next()
+        .expect("always has a first line")
         .split(": ")
         .nth(1)
         .expect("Always a num list part")
         .split_ascii_whitespace()
-        .chunks(2).into_iter()
+        .chunks(2)
+        .into_iter()
         .map(|mut s| {
-            let start = s.next().expect("Always a start num").parse().expect("Always num");
-            let length: u64 = s.next().expect("Always a length num").parse().expect("Always num");
+            let start = s
+                .next()
+                .expect("Always a start num")
+                .parse()
+                .expect("Always num");
+            let length: u64 = s
+                .next()
+                .expect("Always a length num")
+                .parse()
+                .expect("Always num");
 
-            SeedRange{start, length}
-            
+            SeedRange { start, length }
         })
         .collect_vec();
 
-    let mut maps = input.split("\n\n")
+    let mut maps = input
+        .split("\n\n")
         .skip(1) // Skip initial seed
         .map(|map_func| {
-            let funcs = map_func.lines().skip(1) // Skip title
+            let funcs = map_func
+                .lines()
+                .skip(1) // Skip title
                 .map(Mapping::from_line)
                 .collect_vec();
-            MapFunction {funcs}
+            MapFunction { funcs }
         })
         .map(|map_func| map_func.reverse())
         .collect_vec();
 
     maps.reverse();
 
-    let lowest_ending_that_exists = (0..u64::MAX).find(|ending_id| {
-        let start_id = maps.iter().fold(*ending_id, |id, mapping_func| mapping_func.apply_function(id));
-        initial_seeds.iter().any(|initial| initial.is_in_range(start_id))
-    }).expect("Always one valid num");
+    let lowest_ending_that_exists = (0..u64::MAX)
+        .find(|ending_id| {
+            let start_id = maps.iter().fold(*ending_id, |id, mapping_func| {
+                mapping_func.apply_function(id)
+            });
+            initial_seeds
+                .iter()
+                .any(|initial| initial.is_in_range(start_id))
+        })
+        .expect("Always one valid num");
 
     Some(lowest_ending_that_exists)
 }
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -6,31 +6,41 @@ advent_of_code::solution!(6);
 #[derive(Debug)]
 struct Race {
     time: u64,
-    distance: u64
+    distance: u64,
 }
 
 impl From<(u64, u64)> for Race {
     fn from(value: (u64, u64)) -> Self {
-        Race { time: value.0, distance: value.1 }
+        Race {
+            time: value.0,
+            distance: value.1,
+        }
     }
 }
 
 fn races_from_str(s: &str) -> Vec<Race> {
-
     let mut parts = s.lines();
 
-    let times = parts.next().expect("awlays a time line")
-        .split_ascii_whitespace().skip(1);
+    let times = parts
+        .next()
+        .expect("awlays a time line")
+        .split_ascii_whitespace()
+        .skip(1);
 
-    let distances: std::iter::Skip<std::str::SplitAsciiWhitespace<'_>> = parts.next().expect("awlays a dist line")
-        .split_ascii_whitespace().skip(1);
+    let distances: std::iter::Skip<std::str::SplitAsciiWhitespace<'_>> = parts
+        .next()
+        .expect("awlays a dist line")
+        .split_ascii_whitespace()
+        .skip(1);
 
-    times.zip(distances)
-        .map(|(time, dist)| 
+    times
+        .zip(distances)
+        .map(|(time, dist)| {
             (
-                time.parse().expect("Time always a number"), 
-                dist.parse().expect("Dist always a number")
-            ))
+                time.parse().expect("Time always a number"),
+                dist.parse().expect("Dist always a number"),
+            )
+        })
         .map(Into::<Race>::into)
         .collect_vec()
 }
@@ -38,23 +48,31 @@ fn races_from_str(s: &str) -> Vec<Race> {
 fn race_from_str_no_gaps(s: &str) -> Race {
     let mut parts = s.lines();
 
-    let time = parts.next().expect("awlays a time line")
-        .split_ascii_whitespace().skip(1).join_concat().to_string();
+    let time = parts
+        .next()
+        .expect("awlays a time line")
+        .split_ascii_whitespace()
+        .skip(1)
+        .join_concat()
+        .to_string();
 
-    let distance = parts.next().expect("awlays a dist line")
-        .split_ascii_whitespace().skip(1).join_concat().to_string();
+    let distance = parts
+        .next()
+        .expect("awlays a dist line")
+        .split_ascii_whitespace()
+        .skip(1)
+        .join_concat()
+        .to_string();
 
     Race {
         time: time.parse().expect("Time always a number"),
-        distance: distance.parse().expect("Dist always a number")
+        distance: distance.parse().expect("Dist always a number"),
     }
 }
 
 fn num_ways_to_beat_race(race: &Race) -> u64 {
     (0..=race.time)
-        .map(|button_hold_time| {
-            button_hold_time * (race.time - button_hold_time)
-        })
+        .map(|button_hold_time| button_hold_time * (race.time - button_hold_time))
         .filter(|traveled_distance| *traveled_distance > race.distance)
         .count() as u64
 }
@@ -62,11 +80,7 @@ fn num_ways_to_beat_race(race: &Race) -> u64 {
 pub fn part_one(input: &str) -> Option<u64> {
     let races = races_from_str(input);
 
-    Some(races
-        .iter()
-        .map(num_ways_to_beat_race)
-        .product())
-
+    Some(races.iter().map(num_ways_to_beat_race).product())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
